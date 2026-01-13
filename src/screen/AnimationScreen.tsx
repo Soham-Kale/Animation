9b740b545c68f814f1df3cb9147d011e3352ac17
Soham-Kale <sohamkale3014@gmail.com>
2026-01-13 23:17:43 +0530

    Implement fade in/out animation with buttons in AnimationScreen

--- src/screen/AnimationScreen.tsx
+++ src/screen/AnimationScreen.tsx
@@ -1,19 +1,42 @@
-import { Button, ScrollView, StyleSheet, Text, View } from "react-native"
+import { useRef } from "react";
+import { Animated, Button, ScrollView, StyleSheet, Text, View } from "react-native"
 
 const AnimationScreen: React.FC = () => {
+
+    const fadeAnim = useRef(new Animated.Value(0)).current;
+
+    const handleFadeIn = () => {
+        Animated.timing(fadeAnim, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true
+        }).start();
+    };
+
+    const handleFadeOut = () => {
+        Animated.timing(fadeAnim, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver: true
+        }).start();
+    }
+
     return(
         <ScrollView contentContainerStyle={styles.container}>
             <Text style={styles.header}>Basic Animation Demo</Text>
+            <Text style={styles.header}>Fade In and Fade Out Demo</Text>
 
             {/* fade Animation Demo */}
             <View style={styles.demoContainer}>
-                <View style={[styles.box]}>
-
-                </View>
+                <Animated.View style={[
+                    styles.box,
+                    styles.opacityBox, 
+                    {opacity: fadeAnim}
+                ]}></Animated.View>
 
                 <View style={styles.buttonContainer}>
-                    <Button title='Fade In'/>
-                    <Button title='Fade Out'/>
+                    <Button onPress={handleFadeIn} title='Fade In'/>
+                    <Button onPress={handleFadeOut} title='Fade Out'/>
                 </View>
             </View>
         </ScrollView>
@@ -30,7 +53,7 @@ const styles = StyleSheet.create({
     header: {
         fontSize: 24,
         fontWeight: "bold",
-        margin: 16,
+        marginBottom: 10,
     },
     demoContainer: {
         alignItems: 'center',
@@ -44,12 +67,23 @@ const styles = StyleSheet.create({
         marginTop: 10,
     },
     box: {
-        width: "100%",
-        height: "100%",
+        width: 100,
+        height: 100,
         justifyContent: "center",
         alignItems: "center",
         marginVertical: 10,
-
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5,
+        // backgroundColor: "red",
+    },
+    opacityBox: {
+        backgroundColor: "#3498db"
     }
 });
 
